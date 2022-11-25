a272c5342894f7da8ff0dea2cfddc2c872f8f259
Jonathan Fong <jonathanfong098@gmail.com>
2022-11-24 23:47:51 -0800

    add logout functionality to logout button

--- hydrobud/components/Header.js
+++ hydrobud/components/Header.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import { useRouter } from 'next/router'
 import Link from 'next/link'
-import Alert from './Alert'
+import { logout } from '../services/firebase/firebase-auth'
+import { SIGN_IN_REDIRECT_KEY, clearRedirect } from '../utils/redirect'
 
 const Header = ({children}) => {
+    const router = useRouter()
+
+    const logoutHandler = () => {
+        logout()
+        router.push('/login')
+        clearRedirect(SIGN_IN_REDIRECT_KEY)
+    }
+
     return (
         <div className='flex flex-row justify-center items-center space-x-[4rem] w-screen min-h-[6rem] max-h-[10rem] bg-gradient-to-br from-[#92B4A7] to-[#A9D978]'>
             <div className='flex justify-center items-center'>
@@ -11,7 +21,7 @@ const Header = ({children}) => {
 
             <button>Devices</button>
             <button>Profile</button>
-            <button>Logout</button>
+            <button onClick={logoutHandler}>Logout</button>
         </div>
     )
 }
