50634ddb7ec784c9a7db1458c3f052d08f549f10
Jonathan Fong <jonathanfong098@gmail.com>
2022-11-26 22:29:00 -0800

    change page paths

--- hydrobud/pages/home/index.js
+++ hydrobud/pages/home/index.js
@@ -7,7 +7,7 @@ const Homepage = () => {
             <div className='flex flex-row justify-end h-full'>
                 <div className='flex gap-x-[2rem] mt-[4rem] mr-[2rem]'>
                     <Link href='/login' className='h-[5rem] w-[9rem] bg-[#D7D9DE] font-semibold text-white text-3xl rounded-[2rem] hover:bg-[#BAC0D0] flex justify-center items-center'><p>Login</p></Link>
-                    <Link href='/signup' className='h-[5rem] w-[9rem] bg-[#D7D9DE] font-semibold text-white text-3xl rounded-[2rem] hover:bg-[#BAC0D0] flex justify-center items-center'><p>Sign Up</p></Link>
+                    <Link href='/sign-up' className='h-[5rem] w-[9rem] bg-[#D7D9DE] font-semibold text-white text-3xl rounded-[2rem] hover:bg-[#BAC0D0] flex justify-center items-center'><p>Sign Up</p></Link>
                 </div>
             </div>
             <h1 className='text-center tracking-wider text-9xl text-white font-bold'>HydroBud</h1>
